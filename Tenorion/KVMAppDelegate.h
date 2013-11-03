6d7ada14c5396c584ef1f7361110fde75318f5c0
Kim Merrill <kmerrill27@gmail.com>
2013-11-02 21:53:37 -0700

    Initial Commit

---
+++ Tenorion/KVMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KVMAppDelegate.h
+//  Tenorion
+//
+//  Created by Kimberly Merrill on 11/2/13.
+//  Copyright (c) 2013 kmerrill27. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KVMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TenorionTests/TenorionTests.m
@@ -0,0 +1,34 @@
+//
+//  TenorionTests.m
+//  TenorionTests
+//
+//  Created by Kimberly Merrill on 11/2/13.
+//  Copyright (c) 2013 kmerrill27. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TenorionTests : XCTestCase
+
+@end
+
+@implementation TenorionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
